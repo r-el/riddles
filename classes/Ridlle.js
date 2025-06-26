d9d894f6c6ee32e0ff9d8feceafe35495e17e4ee
Ariel <arieltanami122@gmail.com>
2025-06-26 12:10:56 +0300

    Add Riddle class structure

---
+++ classes/Ridlle.js
@@ -0,0 +1,15 @@
+class Riddle {
+  constructor(id, name, taskDescription, correctAnswer) {
+    this.id = id;
+    this.name = name;
+    this.taskDescription = taskDescription;
+    this.correctAnswer = correctAnswer;
+  }
+  ask() {
+    // TODO:
+    // Displays the riddle
+    // Uses the provided readlind.question function to get user input
+    // compare the given input to the correct answer
+    // Repeats until the correct answer is given
+  }
+}
