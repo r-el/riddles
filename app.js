1b340c7388368c8bc59ea0df4e8861ffc946a6d5
Ariel <arieltanami122@gmail.com>
2025-06-26 16:01:42 +0300

    Test Player in app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+import Player from "./classes/Player.js";
 import Riddle from "./classes/Ridlle.js";
 import AllRiddles from "./riddles/index.js";
 
 // Test riddle
 let r1 = Riddle.fromObject(AllRiddles[0]);
 r1.ask();
+
+let p = new Player("Ariel", [4000, 5000, 6000]); // זמנים במילישניות
+console.log(p);
+p.showStats();
