7b9b38b3c95527097b2fcc9e3d72b65cb35a15ae
Ariel <arieltanami122@gmail.com>
2025-06-26 13:40:36 +0300

    Implement ask method

--- classes/Ridlle.js
+++ classes/Ridlle.js
@@ -1,15 +1,18 @@
-class Riddle {
+import { question } from "readline-sync";
+
+export default class Riddle {
   constructor(id, name, taskDescription, correctAnswer) {
     this.id = id;
     this.name = name;
     this.taskDescription = taskDescription;
     this.correctAnswer = correctAnswer;
   }
+
   ask() {
-    // TODO:
-    // Displays the riddle
-    // Uses the provided readlind.question function to get user input
-    // compare the given input to the correct answer
-    // Repeats until the correct answer is given
+    console.log("Your task is: \n", this.taskDescription);
+    question("Your answer: ", {
+      limit: (input) => input === this.correctAnswer,
+      limitMessage: "Incorrect. Try again!",
+    });
   }
 }
