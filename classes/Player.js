e08d1bfe48aeb03c51705eef48e7883c6d61606f
Ariel <arieltanami122@gmail.com>
2025-06-26 16:00:12 +0300

    -Add showStats method in Player class  -Fix fromObject method to correctly instantiate Player class  -Improve time filtering logic

--- classes/Player.js
+++ classes/Player.js
@@ -1,8 +1,25 @@
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration.js";
+dayjs.extend(duration);
+
 export default class Player {
   constructor(name, times = []) {
     this.name = name;
-    this.times = Array.isArray(times) ? times.filter(Number.isFinite) : []; // durations per riddle
+    this.times = Array.isArray(times) ? times.filter(Number.isFinite) : []; // MS durations per riddle
   }
 
-  static fromObject = (obj) => new Riddle(obj.name, obj.times);
+  // display total and average time
+  showStats() {
+    const count = this.times.length;
+    const totalMs = this.times.reduce((total, time) => total + time, 0);
+    const avgMs = count > 0 ? totalMs / count : 0;
+
+    const totalFormatted = dayjs.duration(totalMs).format("H:mm:ss");
+    const avgFormatted = dayjs.duration(avgMs).format("m:ss");
+
+    console.log(`Total time: ${totalFormatted}`);
+    console.log(`Average time per riddle: ${avgFormatted}`);
+  }
+
+  static fromObject = (obj) => new Player(obj.name, obj.times);
 }
