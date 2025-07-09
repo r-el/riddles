dfa5357d12446246e11147a74c739d7e024dae30
Ariel <arieltanami122@gmail.com>
2025-07-09 17:50:10 +0300

    feat: implement main menu and Add DatabaseManager to GameManager as this.db

--- classes/GameManager.js
+++ classes/GameManager.js
@@ -1,29 +1,63 @@
 import Player from "./Player.js";
 import Riddle from "./Riddle.js";
-import AllRiddles from "../riddles/index.js";
+import DatabaseManager from "./DatabaseManager.js";
 import readline from "readline-sync";
 
 export default class GameManager {
   constructor() {
     // The player object will be initialized when the game starts
     this.player = null;
+    this.db = new DatabaseManager();
   }
 
-  // Initialize the player with a name
+  // The player object will be initialized when the game starts
   initPlayer() {
     const playerName = readline.question("What is your name? ");
     this.player = new Player(playerName);
     return playerName;
   }
 
-  start() {
-    // Greet the user and ask for their name
-    console.log("Welcome to the Riddle Game!");
-    const playerName = this.initPlayer();
-    console.log(`Hello, ${playerName}! Let's start...\n`);
+  async showMainMenu() {
+    while (true) {
+      console.log("\nWhat do you want to do?");
+      console.log("1. Play the game");
+      console.log("2. Create a new riddle");
+      console.log("3. Read all riddles");
+      console.log("4. Update an existing riddle");
+      console.log("5. Delete a riddle");
+      console.log("6. View leaderboard");
+      console.log("7. Exit");
+      const choice = readline.question("Enter your choice: ");
+      switch (choice) {
+        case "1":
+          // TODO: Implement this. this.playGame();
+          console.log("Game not implemented yet.");
+          break;
+        case "2":
+          await this.createRiddle();
+          break;
+        case "3":
+          await this.readAllRiddles();
+          break;
+        case "4":
+          await this.updateRiddle();
+          break;
+        case "5":
+          await this.deleteRiddle();
+          break;
+        case "6":
+          // TODO: Implement this. this.showLeaderboard();
+          console.log("Leaderboard not implemented yet.");
+          break;
+        case "7":
+          console.log("Goodbye!");
+          return;
+        default:
+          console.log("Invalid choice. Try again.");
+      }
+    }
+  }
 
-    // Loop through all riddles
-    for (let i = 0; i < AllRiddles.length; i++) {
       const riddleObj = AllRiddles[i];
 
       // Create a Riddle instance from the riddle object
