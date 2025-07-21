9ea185dea0096d5829ce86350dd457da36d1fb10
Ariel <arieltanami122@gmail.com>
2025-07-21 12:02:02 +0300

    feat: Add Player model to represent game players

---
+++ src/models/Player.js
@@ -0,0 +1,20 @@
+/**
+ * Player model representing a game player
+ */
+export class Player {
+  /**
+   * Create a new Player instance
+   * @param {Object} data - Player data
+   * @param {number} data.id - Player ID
+   * @param {string} data.username - Player's username
+   * @param {number} data.best_time - Player's best solving time
+   * @param {string} data.created_at - Account creation date
+   */
+  constructor(data = {}) {
+    this.id = data.id || null;
+    this.username = data.username || "";
+    this.bestTime = data.best_time || 0;
+    this.createdAt = data.created_at ? new Date(data.created_at) : null;
+  }
+
+}
