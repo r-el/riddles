14adc1d2b1bc378dd747fc409db1541e5215f008
Ariel <arieltanami122@gmail.com>
2025-07-21 12:06:06 +0300

    feat: Add Riddle model to represent a single riddle

---
+++ src/models/Riddle.js
@@ -0,0 +1,22 @@
+/**
+ * Riddle model representing a single riddle
+ */
+export class Riddle {
+  /**
+   * Create a new Riddle instance
+   * @param {Object} data - Riddle data
+   * @param {string} data._id - MongoDB ObjectId
+   * @param {string} data.question - The riddle question
+   * @param {string} data.answer - The correct answer
+   * @param {string} data.level - Difficulty level (easy, medium, hard)
+   * @param {string} data.createdAt - Creation date
+   */
+  constructor(data = {}) {
+    this.id = data._id || null;
+    this.question = data.question || "";
+    this.answer = data.answer || "";
+    this.level = data.level || "medium";
+    this.createdAt = data.createdAt ? new Date(data.createdAt) : null;
+  }
+
+}
